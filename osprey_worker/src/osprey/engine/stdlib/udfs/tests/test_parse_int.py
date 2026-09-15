0382b9ffec88e2cff54cd851e31d911f7409e4e3
Leon Shi <101139283+cmttt@users.noreply.github.com>
2026-09-14 20:48:06 -0700

    Add SHA-256 integer UDF (#62)
    * Add SHA-256 integer UDF
    
    * Register ParseInt in stdlib
    
    * Tighten SHA-256 integer UDF docs

--- osprey_worker/src/osprey/engine/stdlib/udfs/tests/test_parse_int.py
+++ osprey_worker/src/osprey/engine/stdlib/udfs/tests/test_parse_int.py
@@ -2,6 +2,7 @@ import pytest
 from osprey.engine.conftest import ExecuteFunction
 from osprey.engine.stdlib.udfs.parse_int import ParseInt
 from osprey.engine.udf.registry import UDFRegistry
+from osprey.worker._stdlibplugin.udf_register import register_udfs
 
 pytestmark = [pytest.mark.use_udf_registry(UDFRegistry.with_udfs(ParseInt))]
 
@@ -15,3 +16,8 @@ def test_parse_int(execute: ExecuteFunction, s: str, expected: int) -> None:
 def test_parse_int_invalid(execute: ExecuteFunction) -> None:
     data = execute('Result = ParseInt(s="ABC")')
     assert data == {'Result': None}
+
+
+def test_parse_int_is_registered_in_stdlib() -> None:
+    registry = UDFRegistry.with_udfs(*register_udfs())
+    assert registry.get('ParseInt') is ParseInt

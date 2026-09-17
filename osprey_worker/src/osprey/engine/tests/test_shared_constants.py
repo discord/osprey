d1e0c1a678fe6cd94648191573f12a5d88fb717c
Billy Guo <william.guo@discordapp.com>
2026-09-17 22:46:48 +0000

    Fix provisional account entity type value

--- osprey_worker/src/osprey/engine/tests/test_shared_constants.py
+++ osprey_worker/src/osprey/engine/tests/test_shared_constants.py
@@ -2,4 +2,4 @@ from osprey.engine.shared_constants import OspreyEntityTypes
 
 
 def test_provisional_account_entity_type_value() -> None:
-    assert OspreyEntityTypes.PROVISIONAL_ACCOUNT == 'ProvisionalAccount'
+    assert OspreyEntityTypes.PROVISIONAL_ACCOUNT == 'Provisional_Account'
